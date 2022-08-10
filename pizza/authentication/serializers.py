0575957878b4a343d067f4a62f8cbac004cbe52d
rizwan183 <r.ansari12996@gmail.com>
2022-08-10 17:23:34 +0530

    first commit

---
+++ pizza/authentication/serializers.py
@@ -0,0 +1,38 @@
+from .models import MyUser
+from rest_framework import serializers, status
+from rest_framework.validators import ValidationError
+from django.contrib.auth.hashers import make_password
+from phonenumber_field.serializerfields import PhoneNumberField
+
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=40, allow_blank=True)
+    email = serializers.EmailField(max_length=80, allow_blank=False)
+    phone_number = PhoneNumberField(allow_null=False, allow_blank=False)
+    password = serializers.CharField(allow_blank=False, write_only=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['id', 'username', 'email', 'phone_number', 'password']
+
+    def validate(self, attrs):
+        email = MyUser.objects.filter(username=attrs.get('username')).exists()
+
+        if email:
+            raise ValidationError(detail="User with email exists", code=status.HTTP_403_FORBIDDEN)
+
+        username = MyUser.objects.filter(username=attrs.get('username')).exists()
+
+        if username:
+            raise ValidationError(detail="User with username exists", code=status.HTTP_403_FORBIDDEN)
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        new_user = MyUser(**validated_data)
+
+        new_user.password = make_password(validated_data.get('password'))
+
+        new_user.save()
+
+        return new_user
